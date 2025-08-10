e539bf1457971052c3f58086bf07404271903d47
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2025-08-10 16:36:22 +0200

    Update Blood.java

--- src/main/java/me/xelades/dev/blood/Blood.java
+++ src/main/java/me/xelades/dev/blood/Blood.java
@@ -3,41 +3,46 @@ package me.xelades.dev.blood;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Blood extends JavaPlugin implements Listener {
+
     @Override
     public void onEnable() {
+        // Register the listener to listen to events
         getServer().getPluginManager().registerEvents(this, this);
     }
-    //@Override
-    //public void onDisable() {
-    //}
 
     @EventHandler
-    public void onHit (EntityDamageByEntityEvent event) {
-        Location bloodLocation = event.getEntity().getLocation();
+    public void onHit(EntityDamageByEntityEvent event) {
+        // Ensure the event is dealing damage to an entity
+        if (event.getEntity() == null || event.getDamager() == null) {
+            return;
+        }
 
+        // Get the location where the blood particles will spawn
+        Location bloodLocation = event.getEntity().getLocation();
         bloodLocation.setY(bloodLocation.getY() + (event.getEntity().getHeight() / 1.5));
 
-        Double particleCount;
-        Double particleMultiplier = 5.0;
-        Double particleMax = 50.0;
-        Double particleRange = 0.4;
+        // Particle variables
+        double particleMultiplier = 5.0;
+        double particleMax = 50.0;
+        double particleRange = 0.4;
 
-        if (event.getFinalDamage() * particleMultiplier > particleMax) {
-            particleCount = particleMax;
-        }
-        else if (event.getFinalDamage() * particleMultiplier < 0) {
-            particleCount = 0.0;
-        }
-        else {
-            particleCount = event.getFinalDamage() * particleMultiplier;
+        // Calculate particle count based on damage dealt
+        double particleCount = Math.min(event.getFinalDamage() * particleMultiplier, particleMax);
+        if (particleCount < 0) {
+            particleCount = 0;
         }
 
-        event.getEntity().getWorld().spawnParticle(Particle.BLOCK_CRACK, bloodLocation, particleCount.intValue(), particleRange, particleRange, particleRange, Material.REDSTONE_BLOCK.createBlockData());
+        // BlockData for REDSTONE_BLOCK particle
+        BlockData blockData = Material.REDSTONE_BLOCK.createBlockData();
+
+        // Spawn the blood particle effect at the calculated location
+        event.getEntity().getWorld().spawnParticle(Particle.BLOCK_CRACK, bloodLocation, (int) particleCount, particleRange, particleRange, particleRange, blockData);
     }
 }
